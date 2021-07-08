35e8b76faeacc2dc11b03e7dda2182148f4a7d69
angryreid <onyourwill@gmail.com>
2021-07-08 22:23:24 +0800

    commit

---
+++ mid/errhanding/fileListingServer/web.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"go-learing/mid/errhanding/fileListingServer/fileListing"
+	"net/http"
+	"os"
+)
+
+type appHandler func(writer http.ResponseWriter, request *http.Request) error
+
+func errWrapper(handler appHandler) func(http.ResponseWriter, *http.Request) {
+	return func(writer http.ResponseWriter, request *http.Request) {
+		err := handler(writer, request)
+		if err != nil {
+			code := http.StatusOK
+			switch {
+			case os.IsNotExist(err):
+				code = http.StatusNotFound
+			case os.IsPermission(err):
+				code = http.StatusForbidden
+			default:
+				code = http.StatusInternalServerError
+			}
+			http.Error(
+				writer,
+				http.StatusText(code),
+				code)
+		}
+	}
+}
+
+func main() {
+	http.HandleFunc("/list/", errWrapper(fileListing.HandlerFileList))
+
+	err := http.ListenAndServe(":8888", nil)
+	if err != nil {
+		panic(err)
+	}
+}
