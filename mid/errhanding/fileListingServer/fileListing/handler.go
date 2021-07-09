7405c6c57ec553fdbe55976b8f7cfbdefdb4ca61
angryreid <onyourwill@gmail.com>
2021-07-09 22:51:47 +0800

    Error handler  with correct way.

--- mid/errhanding/fileListingServer/fileListing/handler.go
+++ mid/errhanding/fileListingServer/fileListing/handler.go
@@ -4,12 +4,30 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
+	"strings"
 )
+
+const prefix = "/list/"
+
+type UserError string
+
+func (e UserError) Error() string {
+	//panic("implement me")
+	return e.Message()
+}
+
+func (e UserError) Message() string {
+	//panic("implement me")
+	return string(e)
+}
 
 func HandlerFileList(
 	writer http.ResponseWriter,
 	request *http.Request) error {
-	path := request.URL.Path[len("/list/"):]
+	if strings.Index(request.URL.Path, prefix) != 0 {
+		return UserError("path must start as " + prefix)
+	}
+	path := request.URL.Path[len(prefix):]
 	file, err := os.Open(path)
 	if err != nil {
 		return err
--- mid/errhanding/fileListingServer/web.go
+++ mid/errhanding/fileListingServer/web.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"go-learing/mid/errhanding/fileListingServer/fileListing"
 	"log"
 	"net/http"
@@ -9,11 +10,31 @@ import (
 
 type appHandler func(writer http.ResponseWriter, request *http.Request) error
 
+type UserError interface {
+	error
+	Message() string
+}
+
 func errWrapper(handler appHandler) func(http.ResponseWriter, *http.Request) {
 	return func(writer http.ResponseWriter, request *http.Request) {
+		defer func() {
+			if r := recover(); r != nil {
+				log.Printf("Panic: %v", r)
+				http.Error(writer,
+					http.StatusText(http.StatusInternalServerError),
+					http.StatusInternalServerError)
+			}
+		}()
 		err := handler(writer, request)
-		log.Println("Log Error", err.Error())
 		if err != nil {
+			log.Println("Log Error", err.Error())
+			fmt.Println(err.(UserError))
+			if userError, ok := err.(UserError); ok {
+				http.Error(writer,
+					userError.Message(),
+					http.StatusBadRequest)
+				return
+			}
 			code := http.StatusOK
 			switch {
 			case os.IsNotExist(err):
@@ -32,7 +53,7 @@ func errWrapper(handler appHandler) func(http.ResponseWriter, *http.Request) {
 }
 
 func main() {
-	http.HandleFunc("/list/", errWrapper(fileListing.HandlerFileList))
+	http.HandleFunc("/", errWrapper(fileListing.HandlerFileList))
 
 	err := http.ListenAndServe(":8888", nil)
 	if err != nil {
